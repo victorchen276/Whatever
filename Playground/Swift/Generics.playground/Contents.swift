31934d8936cc10d2c3aed7cd737376ba28460102
Victor Chen <victorchen276@gmail.com>
2020-09-15 22:00:23 +1200

    test commit

--- Playground/Swift/Generics.playground/Contents.swift
+++ Playground/Swift/Generics.playground/Contents.swift
@@ -60,3 +60,4 @@ let doubleIndex = findIndex(of: 9.3, in: [3.14159, 0.1, 0.25])
 // doubleIndex is an optional Int with no value, because 9.3 isn't in the array
 let stringIndex = findIndex(of: "Andrea", in: ["Mike", "Malcolm", "Andrea"])
 // stringIndex is an optional Int containing a value of 2
+//test
